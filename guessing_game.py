ff9088e22cc3e14298cdcdbecabc94b7efe21912
robertuptc <robertuptc@gmail.com>
2022-05-31 14:49:42 -0400

    finished exercise

--- guessing_game.py
+++ guessing_game.py
@@ -1,1 +1,19 @@
 class GuessingGame():
+    def __init__(self, answer):
+        self.answer = answer
+
+    def guess(self, user_guess):
+        self.user_guess = user_guess
+        if user_guess < self.answer :
+            return 'low'
+        elif user_guess > self.answer :
+            return 'high'
+        else:
+            return "correct"
+    
+    def solved(self):
+        return self.user_guess == self.answer
+
+game = GuessingGame(10)
+print(game.guess(10))
+print(game.solved())
